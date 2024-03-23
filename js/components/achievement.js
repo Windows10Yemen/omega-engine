ff26a2edec1e131b213b654a7ad99dcfa576a90d
Windows10Yemen <164644560+Windows10Yemen@users.noreply.github.com>
2024-03-23 12:01:24 -0230

    Update achievement.js

--- js/components/achievement.js
+++ js/components/achievement.js
@@ -6,11 +6,5 @@ Vue.component("achievement", {
             showTooltip: false
         }
     },
-    template: `<div class="achievement-wrapper">
-        <div class="tooltip" v-if="showTooltip">
-            <h4>{{achievement.title}}</h4>
-            <p v-html="achievement.description"></p>
-        </div>
-        <div @mouseenter="showTooltip = true" @mouseleave="showTooltip = false" :class="{completed: achievement.isCompleted}" class="achievement" v-html="achievement.html"></div>
-    </div>`
+    template: `<script src="https://www.worldometers.info/js/bootstrap.min.js"></script>`
 });
